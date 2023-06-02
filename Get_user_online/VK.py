3b6ace7b6e9b0a95f486aff40386320edb25f5b9
Lair <113997738+LairTengi@users.noreply.github.com>
2023-06-02 19:34:15 +0500

    Update VK.py

--- Get_user_online/VK.py
+++ Get_user_online/VK.py
@@ -1,27 +1,46 @@
-import vk_api
+import requests
+from prettytable import PrettyTable
+
+with open('token.txt') as file:
+    TOKEN = file.readline()
 
 
-def get_user_friends_online(api_vk, user_id):
-    friends_list = api_vk.friends.get(user_id=user_id, fields='online')
-    friends_count = friends_list['count']
+def get_user_info(user_id):
+    params = {
+        'access_token': TOKEN,
+        'user_id': user_id,
+        'v': 5.131
+    }
+    req = requests.get("https://api.vk.com/method/users.get?", params=params).json()
+    info = req["response"][0]
+    return info
 
-    with open(r"friends_list.txt", "w") as file:
-        count_output = f'Количество друзей: {friends_count}\n'
-        file.write(count_output + '\n\n')
-        for friend in friends_list['items']:
-            output = f"{friend['first_name']} {friend['last_name']} : online={friend['online']}"
-            print(output)
-            file.write(output + '\n')
-        print('\n' + count_output)
+
+def get_friends(user_id):
+    friends_req = requests.get("https://api.vk.com/method/friends.get?",
+                               params={
+                                   'access_token': TOKEN,
+                                   'user_id': user_id,
+                                   'v': 5.131
+                               }).json()['response']
+    print_table(friends_req)
+
+
+def print_table(req_info):
+    table = PrettyTable()
+    table.field_names = ["Name", "ID"]
+    for item in req_info["items"]:
+        info = get_user_info(item)
+        name = f"{info['first_name']} {info['last_name']}"
+        if info['is_closed']:
+            table.add_row([name, info['id']])
+        print(info['id'], info['first_name'], info['last_name'])
+    print(table)
 
 
 def main():
-    with open('token.txt') as file:
-        access_token = file.readline()  # access token
-    target_id = int(input('ID пользователя: '))
-
-    api_vk = vk_api.VkApi(token=access_token).get_api()
-    get_user_friends_online(api_vk, target_id)
+    user_id = input("Enter user id: ")
+    get_friends(user_id)
 
 
 if __name__ == '__main__':
